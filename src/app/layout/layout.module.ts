1354272fea57ed5f7e2a6543cb263cc42f41519e
User <erard.miho@gmail.com>
2021-03-16 12:55:10 +0100

    layout module

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LayoutRoutingModule } from './layout-routing.module';
+import { FooterComponent } from './footer/footer.component';
+import { MenuComponent } from './menu/menu.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { FullLayoutComponent } from './full-layout/full-layout.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [FooterComponent, MenuComponent, PageNotFoundComponent, FullLayoutComponent],
   imports: [
     CommonModule,
     LayoutRoutingModule
