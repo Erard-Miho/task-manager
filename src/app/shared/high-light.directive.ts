0623316094996385501c2f6b83463e46e95e73a6
User <erard.miho@gmail.com>
2021-03-16 13:51:58 +0100

    changes

--- src/app/shared/high-light.directive.ts
+++ src/app/shared/high-light.directive.ts
@@ -1,10 +1,25 @@
-import { Directive } from '@angular/core';
-
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 @Directive({
   selector: '[appHighLight]'
 })
 export class HighLightDirective {
 
-  constructor() { }
+  constructor(private el: ElementRef) { }
 
+  private highlight(color: string){
+    this.el.nativeElement.style.color = color;
+    // or we can user Rendere2.setStyle and Renderer2.removeStyle on leave
+  }
+
+  @Input('appHighlight') highlightColor: string;
+
+  @HostListener('mouseenter') onMouseEnter(){
+    this.highlight(this.highlightColor);
+  }
+
+  @HostListener('mouseleave') onMouseLeave(){
+    this.highlight(null);
+  }
+
+  
 }
--- src/app/shared/list/list.component.ts
+++ src/app/shared/list/list.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input,TemplateRef } from '@angular/core';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss']
 })
-export class ListComponent implements OnInit {
+export class ListComponent {
+
+  @Input() items: Array<any>;
+  @Input() template: TemplateRef<{ item }>;
+  @Input() itemStyleClass: string | Array<string>;
+  @Input() groupStyleClass: string | Array<string>;
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
