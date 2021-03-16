f69a8e2eb81d63b0d288e3aceb3267f2917a779d
User <erard.miho@gmail.com>
2021-03-16 12:50:09 +0100

    tasks module

--- src/app/tasks/tasks.module.ts
+++ src/app/tasks/tasks.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TasksRoutingModule } from './tasks-routing.module';
+import { TasksComponent } from './tasks/tasks.component';
+import { TaskModalComponent } from './task-modal/task-modal.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [TasksComponent, TaskModalComponent],
   imports: [
     CommonModule,
     TasksRoutingModule
