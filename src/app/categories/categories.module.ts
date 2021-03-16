7e16efb756bd6d6da07a88c2d08f65ed482cc8a3
User <erard.miho@gmail.com>
2021-03-16 12:49:13 +0100

    categories module

--- src/app/categories/categories.module.ts
+++ src/app/categories/categories.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CategoriesRoutingModule } from './categories-routing.module';
+import { CategoriesComponent } from './categories/categories.component';
+import { CategoryModalComponent } from './category-modal/category-modal.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [CategoriesComponent, CategoryModalComponent],
   imports: [
     CommonModule,
     CategoriesRoutingModule
